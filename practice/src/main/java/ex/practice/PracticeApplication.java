bc922145c2bf70cf60371f34d431f3aa856fa8ee
ppspai <kidone0033@gmail.com>
2023-01-25 23:08:13 +0900

    for Test

---
+++ practice/src/main/java/ex/practice/PracticeApplication.java
@@ -0,0 +1,12 @@
+package ex.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeApplication {
+
+	public static void main(String[] args) {
+		
+	}
+}
